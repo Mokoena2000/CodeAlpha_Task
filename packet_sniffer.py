824c33fa47fca5868dacac1817549eea0d38fba9
Moholeng <moholengmokoena2@gmail.com>
2025-06-21 13:24:47 +0200

    checking if the packet has an ip layer using scapy built in functions extracting the ip layer and using a formated string to print the source, destination and protocol

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -3,3 +3,16 @@
 from scapy.all import sniff, IP
 
 # Define a callback function that processes each packet captured
+
+def packet_callback(packet):
+    # check if the packet has an IP layer
+
+    if IP in packet:
+        ip_layer = packet(IP) # Extract the IP layer from the packet
+
+        # Print the source and destination IP addresses and the protocol number
+
+        print(f"Source: {ip_layer.src} -> Destination: {ip_layer.dst} | Protocol: {ip_layer.proto}")
+
+        
+         
